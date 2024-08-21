29a6f583c9b8508437f01cf4ee72d5c16ec3da2f
AstroGoat <lucashuguet31@gmail.com>
2024-08-21 03:28:42 +0200

    add company and web support

--- init.el
+++ init.el
@@ -169,19 +169,6 @@
 (use-package rust-mode
   :ensure t)
 
-(use-package corfu
-  :ensure t
-  :init
-  (global-corfu-mode)
-  :custom
-  (corfu-auto t)
-  :bind
-  (:map corfu-map
-        ("TAB" . corfu-next)
-        ([tab] . corfu-next)
-        ("S-TAB" . corfu-previous)
-        ([backtab] . corfu-previous)))
-
 (use-package emacs
   :custom
   (tab-always-indent 'complete)
@@ -391,6 +378,39 @@
 
 (use-package dockerfile-mode
   :ensure t)
+
+(use-package company :ensure t)
+
+(use-package web-mode :ensure t)
+
+(use-package flycheck :ensure t)
+
+(use-package tide :ensure t)
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+(setq company-tooltip-align-annotations t)
+
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+(require 'web-mode)
+
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+
+(flycheck-add-mode 'typescript-tslint 'web-mode)
 
 (nvmap :states '(normal visual motion emacs) :keymaps 'override :prefix "SPC"
   "f" '(:which-key "file")
