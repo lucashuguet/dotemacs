58d01a02b68063ab3e5ed86a8c8e0816afced53b
AstroGoat <lucashuguet31@gmail.com>
2024-10-22 13:57:10 +0200

    fix dashboard

--- custom.el
+++ custom.el
@@ -3,8 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4ade6b630ba8cbab10703b27fd05bb43aaf8a3e5ba8c2dc1ea4a2de5f8d45882" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14" "d6b934330450d9de1112cbb7617eaf929244d192c4ffb1b9e6b63ad574784aad" "7c28419e963b04bf7ad14f3d8f6655c078de75e4944843ef9522dbecfcd8717d" "34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec" default))
  '(package-selected-packages
-   '(yaml-mode yasnippet-snippets which-key web-mode vertico unicode-fonts treemacs-nerd-icons treemacs-evil toc-org rust-mode peep-dired org-superstar org-roam org-auto-tangle nix-mode nerd-icons-dired nerd-icons-corfu magit general evil-vimish-fold evil-org evil-commentary evil-collection evil-anzu elpy doom-themes doom-modeline dockerfile-mode diredfl dashboard corfu cape)))
+   '(treemacs yasnippet org-mode yaml-mode yasnippet-snippets which-key web-mode vertico unicode-fonts treemacs-nerd-icons treemacs-evil toc-org rust-mode peep-dired org-superstar org-roam org-auto-tangle nix-mode nerd-icons-dired nerd-icons-corfu magit general evil-vimish-fold evil-org evil-commentary evil-collection evil-anzu elpy doom-themes doom-modeline dockerfile-mode diredfl dashboard corfu cape))
+ '(package-vc-selected-packages '((org-mode :url "https://code.tecosaur.net/tec/org-mode"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
