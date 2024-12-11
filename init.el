1214b4fed3778e1624e105e2cd0a869fbe3d27fb
AstroGoat <lucashuguet31@gmail.com>
2024-12-11 18:03:17 +0100

    fix org roam ui opening twice when using `SPC a r`

--- init.el
+++ init.el
@@ -376,7 +376,7 @@
   (setq org-roam-ui-sync-theme t)
   (setq org-roam-ui-follow t)
   (setq org-roam-ui-update-on-save t)
-  (setq org-roam-ui-open-on-start t))
+  (setq org-roam-ui-open-on-start nil))
 
 (defun disable-all-themes ()
   "disable all active themes."
